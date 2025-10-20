6d1ebe860c2fa030944712a249fa66d8e2a2d085
benidene <dlgusehd09@naver.com>
2025-10-20 23:15:20 +0900

    체크표시 아이콘 크기 키움

--- app/components/ImageModal.tsx
+++ app/components/ImageModal.tsx
@@ -177,7 +177,7 @@ export default function ImageModal({ imageUrl, company, program, onClose }: Imag
             {checkMarks.map((mark, index) => (
               <div
                 key={index}
-                className="absolute flex items-center justify-center w-8 h-8 bg-green-500 rounded-full border-2 border-black shadow-lg"
+                className="absolute flex items-center justify-center w-12 h-12 bg-green-500 rounded-full border-2 border-black shadow-lg" // 크기 증가
                 style={{
                   left: `${mark.x}px`,
                   top: `${mark.y}px`,
@@ -186,7 +186,7 @@ export default function ImageModal({ imageUrl, company, program, onClose }: Imag
                   userSelect: 'none',
                 }}
               >
-                <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-12 h-12 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"> // 크기 증가
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M5 13l4 4L19 7"></path>
                 </svg>
               </div>
